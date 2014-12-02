72029e9a3891b60875ae946dd08731d9935247f8
Daniel Hollands <daniel.hollands@intercity-uk.com>
2014-12-02 12:06:09 +0000

    it green

--- spec/lib/hero_spec.rb
+++ spec/lib/hero_spec.rb
@@ -2,8 +2,10 @@ require 'spec_helper'
 require_relative '../../lib/hero'
 
 describe Hero do
+  let(:dicepool) { double('dicepool') }
+
   describe 'default attributes' do
-    let(:hero) { Hero.new }
+    let(:hero) { Hero.new dicepool: dicepool }
 
     it 'has default strength equal to 3' do
       expect(hero.strength).to eq(3)
@@ -11,13 +13,31 @@ describe Hero do
     it 'has default health equal to 10' do
       expect(hero.health).to eq 10
     end
-    it 'can be initialized with custom strength' do
-      hero = Hero.new strength: 5
-      expect(hero.strength).to eq 5
+  end
+
+  it 'can be initialized with custom strength' do
+    hero = Hero.new strength: 5, dicepool: dicepool
+    expect(hero.strength).to eq 5
+  end
+  it 'can be initialized with custom health' do
+    hero = Hero.new health: 8, dicepool: dicepool
+    expect(hero.health).to eq 8
+  end
+
+  describe 'attack action' do
+    it 'succeeds' do
+      allow(dicepool).to receive(:skill_check).and_return(3)
+      hero    = Hero.new dicepool: dicepool
+      monster = double('monster', toughness: 2)
+
+      expect(hero.attack(monster)).to be true
     end
-    it 'can be initialized with custom health' do
-      hero = Hero.new health: 8
-      expect(hero.health).to eq 8
+    it 'fails' do
+      allow(dicepool).to receive(:skill_check).and_return(1)
+      hero    = Hero.new dicepool: dicepool
+      monster = double('monster', toughness: 2)
+
+      expect(hero.attack(monster)).to be false
     end
   end
 end
