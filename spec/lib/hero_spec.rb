6afbc4afdf39e0914ff6afa86ccf02b8a1ff65e0
Daniel Hollands <daniel.hollands@intercity-uk.com>
2014-12-02 14:08:01 +0000

    Updated hero class

--- spec/lib/hero_spec.rb
+++ spec/lib/hero_spec.rb
@@ -2,15 +2,39 @@ require 'spec_helper'
 require_relative '../../lib/hero'
 
 describe Hero do
-  describe 'default attributes' do
-    let(:hero) { Hero.new }
+  let(:hero) { Hero.new }
 
+  describe 'default attributes' do
     it 'has default strength equal to 3' do
       expect(hero.strength).to eq(3)
     end
     it 'has default health equal to 10' do
       expect(hero.health).to eq 10
     end
+    it 'has default gold equal to 0' do
+      expect(hero.gold).to eq 0
+    end
+    it 'has default exp equal to 0' do
+      expect(hero.exp).to eq 0
+    end
+  end
+
+  it 'can be damaged' do
+    hero.damage(3)
+    hero.damage(2)
+    expect(hero.health).to eq 5
+  end
+
+  it 'gains gold' do
+    hero.gain_gold(10)
+    hero.gain_gold(5)
+    expect(hero.gold).to eq 15
+  end
+
+    it 'gains exp' do
+    hero.gain_exp(10)
+    hero.gain_exp(5)
+    expect(hero.exp).to eq 15
   end
 
   it 'can be initialized with custom strength' do
