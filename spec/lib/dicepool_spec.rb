3f073b3fac861b65b5afa40b1577b070a5e9242e
Daniel Hollands <daniel.hollands@intercity-uk.com>
2014-12-02 16:22:46 +0000

    refactor dicepool test

--- spec/lib/dicepool_spec.rb
+++ spec/lib/dicepool_spec.rb
@@ -3,14 +3,13 @@ require_relative '../../lib/dicepool'
 
 describe Dicepool do
   describe 'skill_check' do
+    let(:dicepool) { Dicepool.new }
     it 'returns true if number of successes is more than difficulty' do
-      dicepool = Dicepool.new
       allow(dicepool).to receive(:roll_die).and_return(5)
 
       expect(dicepool.skill_check(3, 2)).to be true
     end
     it 'returns false if number of successes is less than difficulty' do
-      dicepool = Dicepool.new
       allow(dicepool).to receive(:roll_die).and_return(2)
 
       expect(dicepool.skill_check(3, 2)).to be false
