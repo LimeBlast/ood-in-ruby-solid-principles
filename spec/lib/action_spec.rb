aba4cd689f10aa42b14f307a663c55d9c4b6f841
Daniel Hollands <daniel.hollands@intercity-uk.com>
2014-12-03 11:12:35 +0000

    added test for action

--- spec/lib/action_spec.rb
+++ spec/lib/action_spec.rb
@@ -22,4 +22,21 @@ describe Action do
   let(:action) { TestAction.new hero, dicepool }
 
   it_behaves_like 'action'
+
+  it 'requires action attributes to be implemented' do
+    expect { Action.new hero, dicepool }.to raise_exception
+  end
+
+  describe 'activate' do
+    it 'sends success message if skill check is successful' do
+      allow(dicepool).to receive(:skill_check).and_return(true)
+      expect(action).to receive(:success)
+      action.activate(monster)
+    end
+    it 'sends failure message if skill check is not successful' do
+      allow(dicepool).to receive(:skill_check).and_return(false)
+      expect(action).to receive(:failure)
+      action.activate(monster)
+    end
+  end
 end
